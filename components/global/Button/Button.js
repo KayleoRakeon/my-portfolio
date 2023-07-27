2be4c806c30c09a7168daeef21162cdf85242c98
Benjamin Bourgouin <2130004@isi-montreal.com>
2023-07-27 19:45:26 -0400

    change made on the Button component, to personnalize horizontal alignement

--- components/global/Button/Button.js
+++ components/global/Button/Button.js
@@ -2,8 +2,28 @@
 import classes from './Button.module.css';
 
 function Button({ ...props }) {
+	// Variables
+	let justifyContent;
+	switch (props.align) {
+		case 'left':
+			justifyContent = 'flex-start';
+			break;
+		case 'center':
+			justifyContent = 'center';
+			break;
+		case 'right':
+			justifyContent = 'flex-end';
+			break;
+	}
+
 	return (
-		<a href={props.target} className={classes.Link}>
+		<a
+			href={props.target}
+			className={classes.Link}
+			style={{
+				justifyContent,
+			}}
+		>
 			<button className={classes.Button}>{props.label}</button>
 		</a>
 	);
