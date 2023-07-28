c5d83bb6877945c490a44b473e97c9d2063e9d01
Benjamin Bourgouin <2130004@isi-montreal.com>
2023-07-28 09:12:45 -0400

    targeting contact section by adding an id

--- components/home/Contact/Contact.js
+++ components/home/Contact/Contact.js
@@ -2,7 +2,11 @@
 import classes from './Contact.module.css';
 
 function Contact() {
-	return <h1>Contact</h1>;
+	return (
+		<section id="contact">
+			<h1>Contact</h1>
+		</section>
+	);
 }
 
 export default Contact;
