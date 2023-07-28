adb755a3068ba12f3cd3592893b6e10e4210b911
Benjamin Bourgouin <2130004@isi-montreal.com>
2023-07-28 12:36:38 -0400

    Home > Last project section done with styling

--- components/home/LastProject/LastProject.js
+++ components/home/LastProject/LastProject.js
@@ -8,7 +8,7 @@ import Button from '@/components/global/Button/Button';
 function LastProject() {
 	// Variable
 	const lastProjectDatas = {
-		title: 'Kadaboo Le Projet',
+		title: 'Kadaboo',
 		image: 'kadaboo_1.jpg',
 		description: [
 			'Kadaboo est une application destinée aux proffessionnel.le.s du milieu de l’éducation à la petite enfance. Le but de Kadaboo est de se comporter comme une banque d’activité pédagogique à réaliser avec les enfants, en fonction de différents thèmes, groupes d’âges, et/ou buts pédagogiques.',
@@ -27,23 +27,28 @@ function LastProject() {
 			<article className={classes.LastProjectContainer}>
 				<div className={classes.ProjectInfos}>
 					<h3>{lastProjectDatas.title}</h3>
-					{lastProjectDatas.description.map((paragraph) => (
-						<p>{paragraph}</p>
-					))}
+					<div className={classes.Description}>
+						{lastProjectDatas.description.map((paragraph) => (
+							<p>{paragraph}</p>
+						))}
+					</div>
 					<Button
 						label="plus de détails"
 						target={lastProjectDatas.link}
 						align="left"
 					/>
 				</div>
-				<p>
-					{lastProjectDatas.title
-						.toLocaleLowerCase()
-						.trim()
-						.replaceAll(' ', '')}
-				</p>
-				<img
-					src={`./projects/${projectFolder}/${lastProjectDatas.image}`}
+				<div className={classes.ImageContainer}>
+					<img
+						src={`./projects/${projectFolder}/${lastProjectDatas.image}`}
+					/>
+				</div>
+			</article>
+			<article className={classes.More}>
+				<Button
+					label="Voir tous mes projets"
+					target="/projets"
+					align="center"
 				/>
 			</article>
 		</section>
