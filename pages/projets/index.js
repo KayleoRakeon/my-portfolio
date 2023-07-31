5683af8a79a2840b72c6943cc12b22c854ef19f5
Benjamin Bourgouin <2130004@isi-montreal.com>
2023-07-31 19:45:22 -0400

    Adding a missing s

--- pages/projets/index.js
+++ pages/projets/index.js
@@ -5,7 +5,7 @@ function Projets() {
 	return (
 		<section className={classes.Projects}>
 			<p>
-				Mes projets sont un peu timide,
+				Mes projets sont un peu timides,
 				<br />
 				mais promis ils vont arriver très bientôt !
 			</p>
