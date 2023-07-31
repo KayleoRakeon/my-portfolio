e956d890396460aa2e1a35b71f3d8b1adb5ff55d
Benjamin Bourgouin <2130004@isi-montreal.com>
2023-07-31 19:43:44 -0400

    Projects - waiting message done

--- pages/projets/index.js
+++ pages/projets/index.js
@@ -1,5 +1,16 @@
+// Librairie
+import classes from './Projets.module.css';
+
 function Projets() {
-	return <h1>Projets</h1>;
+	return (
+		<section className={classes.Projects}>
+			<p>
+				Mes projets sont un peu timide,
+				<br />
+				mais promis ils vont arriver très bientôt !
+			</p>
+		</section>
+	);
 }
 
 export default Projets;
