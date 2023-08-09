c5cf79b7a42bf4f10da2edf9480662a12bf76245
Benjamin Bourgouin <benjamin.bourgouin.pro@gmail.coml>
2023-08-09 16:11:23 -0400

    Changing fomr to use react-hook-form library

--- components/home/Contact/Contact.js
+++ components/home/Contact/Contact.js
@@ -1,15 +1,75 @@
 // Librairie
 import classes from './Contact.module.css';
+import { useForm } from 'react-hook-form';
 
 // Composant
 import SectionTitle from '@/components/global/SectionTitle/SectionTitle';
-import FormInput from '@/components/global/FormInput/FormInput';
+import Button from '@/components/global/Button/Button';
 
 function Contact() {
+	// Variables
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm();
+
+	// Methodes
+	const formSubmittedHandler = (datas) => {
+		console.log(datas);
+	};
+
 	return (
 		<section id="contact" className={classes.Contact}>
 			<SectionTitle label="me contacter" shape="rectangle" />
-			<form onSubmit={console.log('check')}>
+			<form onSubmit={handleSubmit(formSubmittedHandler)}>
+				<div className={classes.FormInput}>
+					<input
+						type="text"
+						id="name"
+						name="name"
+						placeholder="nom"
+						pattern="^(?! *$)[A-Za-zÀ-ÖØ-öø-ÿ\- ]+$"
+						{...register('nom', {
+							required: true,
+						})}
+					/>
+				</div>
+				<div className={classes.FormInput}>
+					<input
+						type="email"
+						id="email"
+						name="email"
+						placeholder="ton.courriel@mail.com"
+						pattern="^(?! *$)[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+						{...register('email', {
+							required: true,
+						})}
+					/>
+				</div>
+				<div className={classes.FormInput}>
+					<textarea
+						id="message"
+						name="message"
+						placeholder="message"
+						pattern="(?! *$).+"
+						rows={5}
+						{...register('message', {
+							required: true,
+						})}
+					></textarea>
+				</div>
+				<div className={classes.FormInput}>
+					<Button
+						label="envoyer"
+						submit={true}
+						target=""
+						align="center"
+					/>
+				</div>
+			</form>
+
+			{/* <form onSubmit={console.log('check')}>
 				<FormInput
 					type="text"
 					id="name"
@@ -32,7 +92,7 @@ function Contact() {
 					pattern="(?! *$).+"
 				/>
 				<FormInput type="submit" name="envoyer" />
-			</form>
+			</form> */}
 		</section>
 	);
 }
