74a29c5ccb78075b203224452f71e9f1a7762816
Benjamin Bourgouin <benjamin.bourgouin.pro@gmail.com>
2024-02-01 20:08:20 -0500

    adding env variables for database access

--- next.config.js
+++ next.config.js
@@ -5,6 +5,11 @@ const nextTranslate = require('next-translate-plugin');
 
 const nextConfig = nextTranslate({
 	reactStrictMode: true,
+	env: {
+		mongodb_username: 'benjaminbourgouin',
+		mongodb_password: 'fVvaf0mTISxWNvci',
+		mongodb_db: 'projects'
+	}
 });
 
 module.exports = nextConfig;
